30a8ca7fd67cde87972ac588f2862ea8390b409c
mike-kelly-ch <123395274+mike-kelly-ch@users.noreply.github.com>
2023-06-05 10:06:00 +0100

    Revert "Feature/ecct 107 dockerise rea web app"

--- src/routers/indexRouter.ts
+++ src/routers/indexRouter.ts
@@ -1,9 +1,10 @@
 import { Request, Response, Router, NextFunction } from "express";
 import { HomeHandler } from "./handlers/index/home";
+
 const router: Router = Router();
 const routeViews: string = "router_views/index";
 
-router.get("/registered-email-address", async (req: Request, res: Response, next: NextFunction) => {
+router.get("/", async (req: Request, res: Response, next: NextFunction) => {
     const handler = new HomeHandler();
     const viewData = await handler.execute(req, res);
     res.render(`${routeViews}/home`, viewData);
