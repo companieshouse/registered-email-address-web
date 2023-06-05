169c5a83a60e9053527954f9df09c22f4a224664
mike-kelly-ch <123395274+mike-kelly-ch@users.noreply.github.com>
2023-06-05 08:36:14 +0100

    Revert "Feature/ECCT 68 start screen"

--- src/routers/indexRouter.ts
+++ src/routers/indexRouter.ts
@@ -1,15 +1,12 @@
 import { Request, Response, Router, NextFunction } from "express";
-import * as config from "../config";
-import {
-    home,
-    signIn
-} from "./handlers/index";
-
+import { HomeHandler } from "./handlers/index/home";
 const router: Router = Router();
+const routeViews: string = "router_views/index";
 
-router.get(config.LANDING_URL, home.get);
-router.post(config.LANDING_URL, home.post);
-
-router.get(config.SIGN_IN_URL, signIn.get);
+router.get("/registered-email-address", async (req: Request, res: Response, next: NextFunction) => {
+    const handler = new HomeHandler();
+    const viewData = await handler.execute(req, res);
+    res.render(`${routeViews}/home`, viewData);
+});
 
 export default router;
