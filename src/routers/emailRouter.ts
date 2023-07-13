44917e0e9b57a38fd91725bade743b554be78b73
adeharrison-ch <aharrison1@companieshouse.gov.uk>
2023-07-13 15:31:15 +0100

    cleanup - wip

--- src/routers/emailRouter.ts
+++ src/routers/emailRouter.ts
@@ -1,11 +1,10 @@
 import {NextFunction, Request, Response, Router} from "express";
 import {ChangeEmailAddressHandler} from "./handlers/email/changeEmailAddress";
 import * as config from "../config/index";
-import {CHECK_ANSWER_URL, SUBMITTED_URL} from "../config/index";
+import {CHECK_ANSWER_URL, SUBMITTED_URL} from "../config";
 import FormValidator from "../utils/formValidator.util";
 import * as constants from "../constants/app.const";
 
-import {handleGetCheckRequest, handlePostCheckRequest} from "./handlers/email/confirmEmailChange";
 import {ConfirmCompanyHandler} from "./handlers/company/confirm";
 
 const router: Router = Router();
@@ -16,37 +15,36 @@ const errorsConst: string = "errors";
 
 // GET: /change-email-address
 router.get(config.CHANGE_EMAIL_ADDRESS_URL, async (req: Request, res: Response, next: NextFunction) => {
-  const formValidator = new FormValidator();
-  const handler = new ChangeEmailAddressHandler(
-    formValidator,
-    req.session?.data.signin_info?.user_profile?.email
-  );
-  await handler.get(req, res).then((viewData) => {
-    res.render(`router_views/email/${config.CHANGE_EMAIL_ADDRESS_URL}`, viewData);
-  });
+    const formValidator = new FormValidator();
+    const handler = new ChangeEmailAddressHandler(
+        formValidator,
+        req.session?.data.signin_info?.user_profile?.email
+    );
+    await handler.get(req, res).then((viewData) => {
+        res.render(`router_views/email/${config.CHANGE_EMAIL_ADDRESS_URL}`, viewData);
+    });
 });
 
 // POST: /change-email-address
 router.post(config.CHANGE_EMAIL_ADDRESS_URL, async (req: Request, res: Response, next: NextFunction) => {
-  const formValidator = new FormValidator();
-  const handler = new ChangeEmailAddressHandler(
-    formValidator,
-    req.session?.data.signin_info?.user_profile?.email
-  );
-  await handler.post(req, res).then((viewData) => {
-    if ( Object.prototype.hasOwnProperty.call(viewData, errorsConst) === true)  {
-      res.render(`${routeViews}` + config.CHANGE_EMAIL_ADDRESS_URL, viewData);
-    } else {
-      req.session?.setExtraData(constants.UPDATED_COMPANY_EMAIL, req.body.changeEmailAddress);
-      res.redirect(config.EMAIL_CHECK_ANSWER_URL);
-    }
-  });
+    const formValidator = new FormValidator();
+    const handler = new ChangeEmailAddressHandler(
+        formValidator,
+        req.session?.data.signin_info?.user_profile?.email
+    );
+    await handler.post(req, res).then((viewData) => {
+        if (Object.prototype.hasOwnProperty.call(viewData, errorsConst)) {
+            res.render(`${routeViews}` + config.CHANGE_EMAIL_ADDRESS_URL, viewData);
+        } else {
+            req.session?.setExtraData(constants.UPDATED_COMPANY_EMAIL, req.body.changeEmailAddress);
+            res.redirect(config.EMAIL_CHECK_ANSWER_URL);
+        }
+    });
 });
 
 // GET: /check-your-answers
 router.get(CHECK_ANSWER_URL, async (req: Request, res: Response, next: NextFunction) => {
-    const handler = new ConfirmCompanyHandler();
-    await handler.get(req, res)
+    await new ConfirmCompanyHandler().get(req, res)
         .then((viewData) => {
             res.render(`${routeViews}` + CHECK_ANSWER_URL, viewData);
         });
@@ -54,28 +52,15 @@ router.get(CHECK_ANSWER_URL, async (req: Request, res: Response, next: NextFunct
 
 // POST: /check-your-answers
 router.post(CHECK_ANSWER_URL, async (req: Request, res: Response, next: NextFunction) => {
-    const handler = new ConfirmCompanyHandler();
-
-    await handler.post(req, res)
+    await new ConfirmCompanyHandler().post(req, res)
         .then((viewData) => {
-            if (Object.prototype.hasOwnProperty.call(viewData, errorsConst) === true ||
-                Object.prototype.hasOwnProperty.call(viewData, statementErrorsConst) === true) {
+            if (Object.prototype.hasOwnProperty.call(viewData, errorsConst) ||
+                Object.prototype.hasOwnProperty.call(viewData, statementErrorsConst)) {
                 res.render(`${routeViews}` + CHECK_ANSWER_URL, viewData);
             } else {
                 res.render(`${routeViews}` + SUBMITTED_URL, viewData);
             }
         });
-
-    // const viewData = await handlePostCheckRequest(req)
-    //   .then((viewData) => {
-    //
-    //     if (Object.prototype.hasOwnProperty.call(viewData, errorsConst) === true ||
-    //           Object.prototype.hasOwnProperty.call(viewData, statementErrorsConst) === true ) {
-    //       res.render(`${routeViews}` + CHECK_ANSWER_URL, viewData);
-    //     } else {
-    //       res.render(`${routeViews}` + SUBMITTED_URL, viewData);
-    //     }
-    //   });
 });
 
 export default router;
--- src/routers/handlers/company/confirm.ts
+++ src/routers/handlers/company/confirm.ts
@@ -1,145 +1,83 @@
-import { Request, Response } from "express";
-import { GenericHandler } from "./../generic";
-import { CompanyProfile } from "@companieshouse/api-sdk-node/dist/services/company-profile/types";
-import { Session } from "@companieshouse/node-session-handler";
-import { getCompanyProfile } from "../../../services/company/company.profile.service";
-import { buildAddress, formatForDisplay } from "../../../services/company/confirm.company.service";
-import { getCompanyEmail } from "../../../services/company/company.email.service";
-import { logger } from "../../../lib/Logger";
-import * as constants from "../../../constants/app.const";
-import * as validationConstants from "../../../constants/validation.const";
-import * as config from "../../../config/index";
+import {Request, Response} from "express";
+import {GenericHandler} from "../generic";
+import {Session} from "@companieshouse/node-session-handler";
+import {logger} from "../../../lib/Logger";
 import {
-  COMPANY_NUMBER,
-  SUBMISSION_ID,
-  TRANSACTION_CLOSE_ERROR,
-  UPDATED_COMPANY_EMAIL
+    COMPANY_NUMBER,
+    SUBMISSION_ID,
+    TRANSACTION_CLOSE_ERROR,
+    UPDATED_COMPANY_EMAIL
 } from "../../../constants/app.const";
-import {EMAIL_CHANGE_EMAIL_ADDRESS_URL} from "../../../config/index";
+import {EMAIL_CHANGE_EMAIL_ADDRESS_URL} from "../../../config";
 import {createRegisteredEmailAddressResource} from "../../../services/company/createRegisteredEmailAddressResource";
 import {closeTransaction} from "../../../services/transaction/transaction.service";
 
-
 export class ConfirmCompanyHandler extends GenericHandler {
 
-  constructor () {
-    super();
-  }
-
-  async get (req: Request, response: Response): Promise<Object> {
-    logger.info(`GET request to serve company confirm page`);
-
-    const session: Session = req.session as Session;
-    const updatedCompanyEmail: string | undefined = session.getExtraData(UPDATED_COMPANY_EMAIL);
-
-    return {
-      "updatedCompanyEmail": updatedCompanyEmail,
-      backUri: EMAIL_CHANGE_EMAIL_ADDRESS_URL,
-      signoutBanner: true,
-      userEmail: req.session?.data.signin_info?.user_profile?.email
-    };
-  }
-
-  async post (req: Request, response: Response): Promise<any> {
-    logger.info(`POST request to serve company confirm page`);
-
-    const session: Session = req.session as Session;
-    const updatedCompanyEmail = req.session?.getExtraData(UPDATED_COMPANY_EMAIL);
-    const emailConfirmation: string | undefined = req.body.emailConfirmation;
-
-    if (emailConfirmation === undefined) {
-      return {
-        statementError: "You need to accept the registered email address statement",
-        updatedCompanyEmail: updatedCompanyEmail,
-        backUri: EMAIL_CHANGE_EMAIL_ADDRESS_URL,
-        signoutBanner: true,
-        userEmail: req.session?.data.signin_info?.user_profile?.email
-      };
+    constructor() {
+        super();
     }
 
-    const transactionId = session.getExtraData(SUBMISSION_ID);
-    const companyNumber = session.getExtraData(COMPANY_NUMBER);
+    async get(req: Request, response: Response): Promise<Object> {
+        logger.info(`GET request to serve company confirm page`);
 
-    return await createRegisteredEmailAddressResource(session, <string>transactionId, <string>updatedCompanyEmail)
-        .then(async (res) => {
-          return await closeTransaction(session, <string>companyNumber, <string>transactionId)
-              .then((res) => {
-                return {
-                  backUri: EMAIL_CHANGE_EMAIL_ADDRESS_URL,
-                  signoutBanner: true,
-                  userEmail: req.session?.data.signin_info?.user_profile?.email
-                };
-              }).catch((err) => {
-                return {
-                  statementError: err.message,
-                  updatedCompanyEmail: updatedCompanyEmail,
-                  backUri: EMAIL_CHANGE_EMAIL_ADDRESS_URL,
-                  signoutBanner: true,
-                  userEmail: req.session?.data.signin_info?.user_profile?.email
-                };
-              });
-        }).catch((e) => {
-          return {
-            statementError: TRANSACTION_CLOSE_ERROR + companyNumber,
-            updatedCompanyEmail: updatedCompanyEmail,
+        const session: Session = req.session as Session;
+        const updatedCompanyEmail: string | undefined = session.getExtraData(UPDATED_COMPANY_EMAIL);
+
+        return {
+            "updatedCompanyEmail": updatedCompanyEmail,
             backUri: EMAIL_CHANGE_EMAIL_ADDRESS_URL,
             signoutBanner: true,
             userEmail: req.session?.data.signin_info?.user_profile?.email
-          };
-        });
-//     const session: Session = req.session as Session;
-//     const updatedCompanyEmail = req.session?.getExtraData(UPDATED_COMPANY_EMAIL);
-//     const emailConfirmation: string | undefined = req.body.emailConfirmation;
-//     if (emailConfirmation === undefined) {
-//         return {
-//             statementError: "You need to accept the registered email address statement",
-//             updatedCompanyEmail: updatedCompanyEmail,
-//             backUri: EMAIL_CHANGE_EMAIL_ADDRESS_URL,
-//             signoutBanner: true,
-//             userEmail: req.session?.data.signin_info?.user_profile?.email
-//         };
-//     }
-//
-//     const transactionId = session.getExtraData(SUBMISSION_ID);
-//     const companyNumber = session.getExtraData(COMPANY_NUMBER);
-//
-//     return await createRegisteredEmailAddressResource(session, <string>transactionId, <string>updatedCompanyEmail)
-//         .then(async () => {
-//             return await closeTransaction(session, <string>companyNumber, <string>transactionId).then(() => {
-//                 return {
-//                     backUri: EMAIL_CHANGE_EMAIL_ADDRESS_URL,
-//                     signoutBanner: true,
-//                     userEmail: req.session?.data.signin_info?.user_profile?.email
-//                 };
-//             }).catch((err) => {
-//                 return {
-//                     statementError: err.message,
-//                     updatedCompanyEmail: updatedCompanyEmail,
-//                     backUri: EMAIL_CHANGE_EMAIL_ADDRESS_URL,
-//                     signoutBanner: true,
-//                     userEmail: req.session?.data.signin_info?.user_profile?.email
-//                 };
-//             });
-//         }).catch((e) => {
-//             return {
-//                 statementError: TRANSACTION_CLOSE_ERROR + companyNumber,
-//                 updatedCompanyEmail: updatedCompanyEmail,
-//                 backUri: EMAIL_CHANGE_EMAIL_ADDRESS_URL,
-//                 signoutBanner: true,
-//                 userEmail: req.session?.data.signin_info?.user_profile?.email
-//             };
-//         });
-  }
+        };
+    }
 
-  buildPageOptions (session: Session, companyProfile: CompanyProfile){
-    const formattedCompanyProfile = formatForDisplay(companyProfile);
-    const address = buildAddress(formattedCompanyProfile);
-    this.viewData.companyProfile = companyProfile;
-    this.viewData.company = formattedCompanyProfile;
-    this.viewData.address = address;
-    this.viewData.userEmail = session.data.signin_info?.user_profile?.email;
-    this.viewData.backUri = config.COMPANY_NUMBER_URL;
-  }
+    async post(req: Request, response: Response): Promise<any> {
+        logger.info(`POST request to serve company confirm page`);
 
+        const session: Session = req.session as Session;
+        const updatedCompanyEmail = req.session?.getExtraData(UPDATED_COMPANY_EMAIL);
+        const emailConfirmation: string | undefined = req.body.emailConfirmation;
 
+        if (emailConfirmation === undefined) {
+            return {
+                statementError: "You need to accept the registered email address statement",
+                updatedCompanyEmail: updatedCompanyEmail,
+                backUri: EMAIL_CHANGE_EMAIL_ADDRESS_URL,
+                signoutBanner: true,
+                userEmail: req.session?.data.signin_info?.user_profile?.email
+            };
+        }
+
+        const transactionId = session.getExtraData(SUBMISSION_ID);
+        const companyNumber = session.getExtraData(COMPANY_NUMBER);
+
+        return await createRegisteredEmailAddressResource(session, <string>transactionId, <string>updatedCompanyEmail)
+            .then(async () => {
+                return await closeTransaction(session, <string>companyNumber, <string>transactionId)
+                    .then(() => {
+                        return {
+                            backUri: EMAIL_CHANGE_EMAIL_ADDRESS_URL,
+                            signoutBanner: true,
+                            userEmail: req.session?.data.signin_info?.user_profile?.email
+                        };
+                    }).catch((err) => {
+                        return {
+                            statementError: err.message,
+                            updatedCompanyEmail: updatedCompanyEmail,
+                            backUri: EMAIL_CHANGE_EMAIL_ADDRESS_URL,
+                            signoutBanner: true,
+                            userEmail: req.session?.data.signin_info?.user_profile?.email
+                        };
+                    });
+            }).catch((e) => {
+                return {
+                    statementError: TRANSACTION_CLOSE_ERROR + companyNumber,
+                    updatedCompanyEmail: updatedCompanyEmail,
+                    backUri: EMAIL_CHANGE_EMAIL_ADDRESS_URL,
+                    signoutBanner: true,
+                    userEmail: req.session?.data.signin_info?.user_profile?.email
+                };
+            });
+    }
 }
