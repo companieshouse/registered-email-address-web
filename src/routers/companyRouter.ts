d7f495340bee102da2c2b00ebd2234d7b35291b8
John Griffiths <jgriffiths2@companieshouse.gov.uk>
2023-07-18 12:07:48 +0100

    ECCT-385 updates following PR

--- src/routers/companyRouter.ts
+++ src/routers/companyRouter.ts
@@ -3,73 +3,87 @@ import { CompanySearchHandlerPost } from "./handlers/company/companySearch";
 import { ConfirmCompanyHandler } from "./handlers/company/confirm";
 import { InvalidCompanyHandler } from "./handlers/company/invalidCompany";
 import { CompanyProfile } from "@companieshouse/api-sdk-node/dist/services/company-profile/types";
-import * as config from "../config/index";
 import { logger } from "../lib/Logger";
 import FormValidator from "../utils/formValidator.util";
 import CompanyNumberSanitizer from "../utils/companyNumberSanitizer";
-import * as constants from "../constants/app.const";
-import * as validationConstants from "../constants/validation.const";
+
+import {
+  COMPANY_CONFIRM_URL,
+  COMPANY_INVALID_PAGE,
+  COMPANY_SEARCH_PAGE,
+  CONFIRM_URL,
+  EMAIL_CHANGE_EMAIL_ADDRESS_URL,
+  INVALID_COMPANY_URL,
+  INVALID_URL,
+  NUMBER_URL,
+  REA_HOME_PAGE,
+  SERVICE_UNAVAILABLE_URL
+} from "../config";
+
+import { COMPANY_PROFILE, COMPANY_NUMBER, INVALID_COMPANY_REASON } from "../constants/app.const";
+
+import { INVALID_COMPANY_SERVICE_UNAVAILABLE } from "../constants/validation.const";
 
 const router: Router = Router();
 const routeViews: string = "router_views/company/";
 const errorsConst: string = "errors";
 const invalidCompanyReason: string = "invalidCompanyReason";
 
-router.get(config.NUMBER_URL, (req: Request, res: Response, next: NextFunction) => {
+router.get(NUMBER_URL, (req: Request, res: Response, next: NextFunction) => {
   logger.info(`GET request to enter company number`);
-  res.render(`${routeViews}` + config.COMPANY_SEARCH_PAGE, { backUri: config.REA_HOME_PAGE, userEmail: req.session?.data.signin_info?.user_profile?.email, signoutBanner: true });
+  res.render(`${routeViews}` + COMPANY_SEARCH_PAGE, { backUri: REA_HOME_PAGE, userEmail: req.session?.data.signin_info?.user_profile?.email, signoutBanner: true });
 });
 
-router.post(config.NUMBER_URL, async (req: Request, res: Response, next: NextFunction) => {
+router.post(NUMBER_URL, async (req: Request, res: Response, next: NextFunction) => {
   const formValidator = new FormValidator();
   const companyNumberSanitizer = new CompanyNumberSanitizer();
   await new CompanySearchHandlerPost(formValidator, companyNumberSanitizer).post(req, res).then((data) => {
     // eslint-disable-next-line no-prototype-builtins
     if (Object.prototype.hasOwnProperty.call(data, errorsConst)) {
-      res.render(`${routeViews}` + config.COMPANY_SEARCH_PAGE, data);
+      res.render(`${routeViews}` + COMPANY_SEARCH_PAGE, data);
     } else {
       // eslint-disable-next-line no-unused-expressions
-      req.session?.setExtraData(constants.COMPANY_PROFILE, data);
-      res.redirect(config.COMPANY_CONFIRM_URL);
+      req.session?.setExtraData(COMPANY_PROFILE, data);
+      res.redirect(COMPANY_CONFIRM_URL);
     }
   });
 });
 
-router.get(config.CONFIRM_URL, async (req: Request, res: Response, next: NextFunction) => {
+router.get(CONFIRM_URL, async (req: Request, res: Response, next: NextFunction) => {
   const handler = new ConfirmCompanyHandler();
   const viewData = await handler.get(req, res);
   // eslint-disable-next-line no-prototype-builtins
   if (Object.prototype.hasOwnProperty.call(viewData, errorsConst)) {
-    res.render(`${routeViews}` + config.COMPANY_SEARCH_PAGE, viewData);
+    res.render(`${routeViews}` + COMPANY_SEARCH_PAGE, viewData);
   } else {
-    res.render(`${routeViews}` + config.CONFIRM_URL, viewData);
+    res.render(`${routeViews}` + CONFIRM_URL, viewData);
   }
 });
 
-router.post(config.CONFIRM_URL, async (req: Request, res: Response, next: NextFunction) => {
-  const companyProfile: CompanyProfile | undefined = req.session?.getExtraData(constants.COMPANY_PROFILE);
+router.post(CONFIRM_URL, async (req: Request, res: Response, next: NextFunction) => {
+  const companyProfile: CompanyProfile | undefined = req.session?.getExtraData(COMPANY_PROFILE);
   if (companyProfile !== undefined) {
-    req.session?.setExtraData(constants.COMPANY_NUMBER, companyProfile.companyNumber);
+    req.session?.setExtraData(COMPANY_NUMBER, companyProfile.companyNumber);
   }
   const handler = new ConfirmCompanyHandler();
   await handler.post(req, res).then((data) => {
     if (Object.prototype.hasOwnProperty.call(data, invalidCompanyReason)) {
-      if (data.invalidCompanyReason === validationConstants.INVALID_COMPANY_SERVICE_UNAVAILABLE) {
-        res.redirect(config.SERVICE_UNAVAILABLE_URL);
+      if (data.invalidCompanyReason === INVALID_COMPANY_SERVICE_UNAVAILABLE) {
+        res.redirect(SERVICE_UNAVAILABLE_URL);
       } else {
-        req.session?.setExtraData(constants.INVALID_COMPANY_REASON, data.invalidCompanyReason);
-        res.redirect(config.INVALID_COMPANY_URL);
+        req.session?.setExtraData(INVALID_COMPANY_REASON, data.invalidCompanyReason);
+        res.redirect(INVALID_COMPANY_URL);
       }
     } else {
-      res.redirect(config.EMAIL_CHANGE_EMAIL_ADDRESS_URL);
+      res.redirect(EMAIL_CHANGE_EMAIL_ADDRESS_URL);
     }
   });
 });
 
-router.get(config.INVALID_URL, async (req: Request, res: Response, next: NextFunction) => {
+router.get(INVALID_URL, async (req: Request, res: Response, next: NextFunction) => {
   const handler = new InvalidCompanyHandler();
   await handler.get(req, res).then((data) => {
-    res.render(`${routeViews}` + config.COMPANY_INVALID_PAGE, data);
+    res.render(`${routeViews}` + COMPANY_INVALID_PAGE, data);
   });
 });
 
