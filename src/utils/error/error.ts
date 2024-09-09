5b5dba205ad92396c7255678cbde595111a831c2
MalJeffCH <mjeff@companieshouse.gov.uk>
2024-09-09 11:03:40 +0100

    SEC-66

--- src/utils/error/error.ts
+++ src/utils/error/error.ts
@@ -1,20 +1,17 @@
-import { Request, Response } from "express";
+import { Request, Response, NextFunction } from "express";
 import { StatusCodes } from "http-status-codes";
 import { CsrfError } from "@companieshouse/web-security-node";
 import { THERE_IS_A_PROBLEM_URL } from "../../config/index";
 
 const pageNotFoundView: string = "partials/error_404";
 
-export const pageNotFound = (req: Request, res: Response) => {
-  return res.status(StatusCodes.NOT_FOUND).render(pageNotFoundView);
-};
 /**
  * This handler catches any CSRF errors thrown within the application.
  * If it is not a CSRF, the error is passed to the next error handler.
  * If it is a CSRF error, it responds with a 403 forbidden status and renders the CSRF error.
  */
 
-export const csrfErrorHandler = (err: CsrfError | Error, req: Request, res: Response, next: Function) => {
+export const csrfErrorHandler = (err: CsrfError | Error, req: Request, res: Response, next: NextFunction) => {
   if (!(err instanceof CsrfError)) {
     return next(err);
   }
@@ -25,4 +22,6 @@ export const csrfErrorHandler = (err: CsrfError | Error, req: Request, res: Resp
   });
 };
 
-export default pageNotFound;
+export const pageNotFound = (req: Request, res: Response) => {
+  return res.status(StatusCodes.NOT_FOUND).render(pageNotFoundView);
+};
