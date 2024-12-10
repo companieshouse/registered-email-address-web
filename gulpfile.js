e42d11a165df85ec2bea9ee5b95d6d97aace9c59
tcbaileych <tbailey@companieshouse.gov.uk>
2024-12-10 15:40:30 +0000

    removed unused govuk-assets gulp task and dependencies; altered build script as a result of no assets to build locally

--- gulpfile.js
+++ gulpfile.js
@@ -1,26 +1,15 @@
 const gulp = require("gulp");
-const dartSass = require("sass");
 const clean = require("gulp-clean");
 
-const dstDirAssets = "./assets/public";
+const dstDir = "./dist";
 
-// Purge all before building
+// Purge 'dist' directory before building
 gulp.task("clean", () => {
-    return gulp.src([dstDirAssets], { read: false, allowEmpty: true })
+    return gulp.src([dstDir], { read: false, allowEmpty: true })
         .pipe(clean());
 });
 
-// Copy the fonts and images from the govuk-frontend package to the public directory
-gulp.task("govuk-assets", () => {
-    return gulp
-        .src(["./node_modules/govuk-frontend/govuk/assets/**/*"])
-        .pipe(gulp.dest(dstDirAssets));
-});
-
-// Binding all tasks together...
-gulp.task("build", gulp.series(["clean", "govuk-assets"]));
-
-gulp.task("serve", gulp.series("build", () => {
+gulp.task("serve", gulp.series("clean", () => {
     exec("npm start", function (err, stdout, stderr) {
         if (err) {
             console.log(err);
