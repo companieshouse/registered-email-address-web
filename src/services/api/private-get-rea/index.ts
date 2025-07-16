19b126e62c39373c8c9313a0d4b6ddf4429b99a6
NeilShadrach <nshadrach@companieshouse.gov.uk>
2025-07-16 09:29:08 +0100

    CC-2672 Updates to allow service to be run in development mode
    CC-2672 Import environment variable in one place
    
    CC-2672 Use private account URL and remove obsolete lines
    
    CC-2672 Remove unused imports
    
    CC-2672 Eliminate unused account URL endpoint

--- src/services/api/private-get-rea/index.ts
+++ src/services/api/private-get-rea/index.ts
@@ -1,42 +1,20 @@
 import PrivateApiClient from "./private_api_client";
-import { API_URL, ACCOUNT_URL } from "./config";
-import { RequestClient, HttpClientOptions, IHttpClient } from "@companieshouse/api-sdk-node/dist/http";
-import Resource from "@companieshouse/api-sdk-node/dist/services/resource";
-import { PRIVATE_API_ERROR } from "../../../constants/app_const";
+import { CHS_INTERNAL_API_KEY, ORACLE_QUERY_API_URL } from "../../../config";
+import { RequestClient, HttpClientOptions } from "@companieshouse/api-sdk-node/dist/http";
 
 /**
  * Creates a new API Client.
- *
- * @param apiKey the api key to use for authentication
- * @param oauthToken a user's oauth token that can be used for authentication
- * @param baseUrl the api base url
  */
-export const createPrivateApiClient = (apiKey?: string, oauthToken?: string, baseUrl: string = API_URL, baseAccountUrl: string = ACCOUNT_URL): PrivateApiClient => {
-  if (apiKey && oauthToken) {
-    throw new Error(PRIVATE_API_ERROR);
-  }
+export const createPrivateApiClient = (): PrivateApiClient => {
 
   // the http client adapter for the api domain
   const apiOptions: HttpClientOptions = {
-    apiKey,
-    baseUrl,
-    oauthToken
+    apiKey: CHS_INTERNAL_API_KEY,
+    baseUrl: ORACLE_QUERY_API_URL,
+    oauthToken: undefined
   };
   const apiHttpClient = new RequestClient(apiOptions);
 
-  // the http client adapter for the account domain
-  const accountOptions: HttpClientOptions = {
-    apiKey: apiKey,
-    baseUrl: baseAccountUrl,
-    oauthToken: oauthToken
-  };
-  const accountHttpClient = new RequestClient(accountOptions);
-
   // the api client
-  return new PrivateApiClient(apiHttpClient, accountHttpClient);
+  return new PrivateApiClient(apiHttpClient);
 };
-
-// exports used by private sdk to provide private services without the need to duplicate configs or http client logic
-export { IHttpClient, HttpClientOptions, RequestClient, API_URL, ACCOUNT_URL, Resource };
-
-export * from "./registered_email_address_service";
